fefbeb5371013b342af42333b33ca8f34e99ae62
NeilMagon <neil.magon97@gmail.com>
2024-04-16 17:46:04 +0200

    feat: get image

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -6,14 +6,19 @@ export default {
     components: {
         SingleCard,
     },
+    data() {
+        return {
+            store
+        };
+    }
 }
 </script>
 
 <template>
     <section class="p-5 ms-bg">
         <div class="container bg-white">
-           <div class="row row-cols-5 pb-3 g-3">
-            <SingleCard></SingleCard>
+           <div class="row row-cols-5 pb-5 g-3">
+            <SingleCard v-for="singleCard in store.cards" :key="singleCard.id" :cardInfo="singleCard"></SingleCard>
            </div>
         </div>
     </section>
--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,17 +1,20 @@
 <script>
 export default {
     name: 'SingleCard',
+    props: {
+        cardInfo: Object
+    }
 }
 </script>
 
 <template>
     <div class="col d-flex flex-column">
         <div class="img-container">
-            <img src="https://images.ygoprodeck.com/images/cards_small/34541863.jpg" alt="">
+            <img :src="cardInfo.card_images[0].image_url_small" :alt="cardInfo.name">
         </div>
         <div class="text-white ms-bg text-center">
-            <h5 class="p-2">gsggrgesg</h5>
-            <h6 class="text-dark p-2">dgrshhb</h6>
+            <h5 class="p-2">{{cardInfo.name }}</h5>
+            <h6 class="text-dark p-2">{{ cardInfo.archetype }}</h6>
         </div>
     </div>
 </template>
