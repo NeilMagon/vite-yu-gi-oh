c330d7c2056c15e218795c950dd56271beb7b213
NeilMagon <neil.magon97@gmail.com>
2024-04-16 17:00:52 +0200

    feat: fix singlecard

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -9,13 +9,16 @@ export default {
         <div class="img-container">
             <img src="https://images.ygoprodeck.com/images/cards_small/34541863.jpg" alt="">
         </div>
-        <div class="text-white"></div>
+        <div class="text-white ms-bg text-center">
+            <h5 class="p-2">gsggrgesg</h5>
+            <h6 class="text-dark p-2">dgrshhb</h6>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
     @use '../style/partials/variables' as *;
-    .img-container{
+    .ms-bg{
         background-color: $main-color;
         img{
             width: 100%;
